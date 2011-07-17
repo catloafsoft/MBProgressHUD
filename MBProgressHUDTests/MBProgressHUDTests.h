82ac9bd633e3bb29168a36b5db1a8baa8c8e1725
Stephane Peter <megastep@megastep.org>
2011-07-16 18:11:23 -0700

    Added static library project, changed demo project to depend on it.

---
+++ MBProgressHUDTests/MBProgressHUDTests.h
@@ -0,0 +1,13 @@
+//
+//  MBProgressHUDTests.h
+//  MBProgressHUDTests
+//
+//  Created by Stéphane Peter on 7/16/11.
+//  Copyright 2011 Catloaf Software, LLC All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MBProgressHUDTests : SenTestCase
+
+@end
---
+++ MBProgressHUDTests/MBProgressHUDTests.m
@@ -0,0 +1,32 @@
+//
+//  MBProgressHUDTests.m
+//  MBProgressHUDTests
+//
+//  Created by Stéphane Peter on 7/16/11.
+//  Copyright 2011 Catloaf Software, LLC. All rights reserved.
+//
+
+#import "MBProgressHUDTests.h"
+
+@implementation MBProgressHUDTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MBProgressHUDTests");
+}
+
+@end
